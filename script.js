1476dc25013b00c835f21c8d9b0d5ca1c76c201c
joperopi <70579397+joperopi@users.noreply.github.com>
2024-08-15 18:35:18 -0300

    Add menu event listeners

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnHat = document.getElementById("hatitem");
+const btnFace = document.getElementById("faceitem");
+const btnTop = document.getElementById("topitem");
+const btnBottom = document.getElementById("bottomitem");
+const btnShoe = document.getElementById("shoeitem");
+const btnPrint = document.getElementById("printitem");
+
+btnHat.addEventListener("click", function(e) {
+    alert("test");
+});
