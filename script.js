1f6b4ab1d75006e3f95918186a049cda7468eb3e
joperopi <70579397+joperopi@users.noreply.github.com>
2024-08-16 11:46:23 -0300

    Add coments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// MENU BUTTONS
 const btnHat = document.getElementById("hatmenu");
 const btnFace = document.getElementById("facemenu");
 const btnTop = document.getElementById("topmenu");
 const btnBottom = document.getElementById("bottommenu");
 const btnShoe = document.getElementById("shoemenu");
 const btnPrint = document.getElementById("printmenu");
+
+// POPUP WINDOWS
 
 
 
